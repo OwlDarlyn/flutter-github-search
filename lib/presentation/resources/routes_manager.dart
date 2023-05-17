59afc6b616b59b077ccff5b8540996ea2af68fd3
OwlDarlyn <imdarlyyn@gmail.com>
2023-05-17 20:29:05 +0300

    add route manager

---
+++ lib/presentation/resources/routes_manager.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'package:potje_test_assignment/presentation/favorite/favorite_view.dart';
+import 'package:potje_test_assignment/presentation/resources/strings_manager.dart';
+import 'package:potje_test_assignment/presentation/search/search_view.dart';
+import 'package:potje_test_assignment/presentation/splash/splash_view.dart';
+
+class Routes {
+  static const String splashRoute = '/';
+  static const String searchRoute = '/search';
+  static const String favoriteRoute = '/favorite';
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.searchRoute:
+        return MaterialPageRoute(builder: (_) => const SearchView());
+      case Routes.favoriteRoute:
+        return MaterialPageRoute(builder: (_) => const FavoriteScreen());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => const Scaffold(
+        body: Center(
+          child: Text(AppStrings.searchApp),
+        ),
+      ),
+    );
+  }
+}
