ae40708bcb8bed62267a6ed0cbcce04ded02ff0c
OwlDarlyn <imdarlyyn@gmail.com>
2023-05-17 20:19:54 +0300

    add value manaher

---
+++ lib/presentation/resources/values_manager.dart
@@ -0,0 +1,14 @@
+class AppMargin {
+  static const double m8 = 8.0;
+  static const double m16 = 16.0;
+  static const double m18 = 18.0;
+  static const double m20 = 20.0;
+  static const double m24 = 24.0;
+}
+
+class AppPadding {
+  static const double p8 = 8.0;
+  static const double p16 = 16.0;
+  static const double p18 = 18.0;
+  static const double p20 = 20.0;
+}
