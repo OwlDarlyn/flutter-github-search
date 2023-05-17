7103f00a291d059fc0e0aa1714661ca1c7bee11e
OwlDarlyn <imdarlyyn@gmail.com>
2023-05-17 20:57:41 +0300

    add my app

---
+++ lib/app/my_app.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'package:potje_test_assignment/presentation/resources/routes_manager.dart';
+
+class MyApp extends StatefulWidget {
+  MyApp._internal();
+  int appState = 0;
+
+  static final MyApp instance = MyApp._internal();
+
+  factory MyApp() => instance;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: RouteGenerator.getRoute,
+      initialRoute: Routes.splashRoute,
+      theme: getApplicationTheme(),
+    );
+  }
+}
