8925f9a27e28354e846318f509ffb5d789dcb10f
OwlDarlyn <imdarlyyn@gmail.com>
2023-05-17 20:14:07 +0300

    add strings manager

---
+++ lib/presentation/resources/strings_manager.dart
@@ -0,0 +1,17 @@
+class AppStrings {
+  static const String searchApp = 'Search App';
+  static const String search = 'Search';
+
+  static const String empty =
+      'You have empty history. Click on search to start journey!';
+  static const String nothingSearch =
+      'Nothing was find for your search. Please check the spelling';
+  static const String nothingFavorite =
+      'You have no favorites. Click on star while searching to add first favorite';
+
+  static const String searchTitle = 'Github repos list';
+  static const String favoriteTitle = 'Favorite repos list';
+
+  static const String searchSubTitle1 = 'Search History';
+  static const String searchSubTitle2 = 'What we have found';
+}
