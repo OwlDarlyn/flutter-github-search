75da87caac22cddaedeeb478ddd1c2f274bfd832
OwlDarlyn <imdarlyyn@gmail.com>
2023-05-17 21:37:46 +0300

    add theme manager

--- lib/presentation/resources/theme_manager.dart
+++ lib/presentation/resources/theme_manager.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
 import 'package:potje_test_assignment/presentation/resources/color_manager.dart';
+import 'package:potje_test_assignment/presentation/resources/styles_manager.dart';
+import 'package:potje_test_assignment/presentation/resources/values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-    primaryColor: ColorManager.main,
+    primaryColor: ColorManager.accentPrimary,
+    disabledColor: ColorManager.placeHolder,
+    colorScheme: ColorScheme.fromSwatch()
+        .copyWith(secondary: ColorManager.accentSecondary),
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.main,
+      elevation: AppSize.s4,
+      titleTextStyle: getHeaderStyle(color: ColorManager.textPrimary),
+    ),
   );
 }
